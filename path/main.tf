83502f0fb26b4f828da0fb3fb1ee348aa8794825
PraveenPulisetty <praveenpulisetty@gmail.com>
2024-07-17 01:40:41 +0530

    Added Data block content

--- path/main.tf
+++ path/main.tf
@@ -2,3 +2,39 @@ resource "azurerm_resource_group" "testrg" {
   name     = "rg-tf-demo"
   location = "eastus"
 }
+
+# Get Resources from a Resource Group
+data "azurerm_resources" "rg-kv" {
+  resource_group_name = "rg-praveen"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "my-kv" {
+  name                        = "terraformkvp"
+  location                    = data.azurerm_resources.rg-kv.location
+  resource_group_name         = data.azurerm_resources.rg-kv.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
